a7e989b2ea6d1498042c1718b84e1008e1fe109d
neo-dev <sei133dirsoa@gmail.com>
2025-11-06 13:18:58 -0300

    Final Solution pt 1

--- Project/src/server.js
+++ Project/src/server.js
@@ -1,6 +1,7 @@
 /*
   Importações
 */
+const fs = require("fs");
 const express = require("express");
 const path = require("path");
 const morgan = require("morgan");
@@ -8,9 +9,19 @@ const morgan = require("morgan");
 const app = express(); // o objeto que representa o servidor é instanciado aqui
 const PORT = process.env.PORT || 3000; // a porta do servidor é 3000
 
+const logDir = path.join(__dirname, "..", "log");
+if (!fs.existsSync(logDir)) {
+  fs.mkdirSync(logDir);
+}
+
+const acessLogStream = fs.createWriteStream(path.join(logDir, "service.log"), {
+  flags: "a",
+});
+
 // middleware
 app.use(morgan("dev")); // morgan provê o logging do backend
-app.use(express.static(path.join(__dirname, "public")));
+app.use(morgan("combined", { stream: acessLogStream })); // cria
+app.use(express.static(path.join(__dirname, "..", "public")));
 // ativa o serviço para prover os sites estáticos do diretório public
 
 app.get("/", (req, res) => {
